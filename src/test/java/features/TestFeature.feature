07f84952e6abef9f3036e5e5ac14183b865cf6e0
DineshArul7 <dinesh.ak37@gmail.com>
2023-04-20 13:52:38 +0530

    Adding Remote Driver

--- src/test/java/features/TestFeature.feature
+++ src/test/java/features/TestFeature.feature
@@ -30,3 +30,4 @@ Scenario Outline: Testing the findStoresWithLocation
 
 
 
+
--- src/test/java/stepDefinitions/CucumberHooks.java
+++ src/test/java/stepDefinitions/CucumberHooks.java
@@ -5,12 +5,24 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 
+import java.net.MalformedURLException;
+
 public class CucumberHooks {
     Base base=new Base();
     @Before
-    public void beforeScenario(Scenario scenario){
+    public void beforeScenario(Scenario scenario) throws MalformedURLException {
+
+        System.out.println(scenario.getName());
+        String browserName="";
+        if(scenario.getName().contains("findStoresWithLocation")||scenario.getName().contains("findProduct")){
+            browserName="chrome";
+        } else  {
+            browserName="firefox";
+        }
+
+
         System.out.println("This will run before the Scenario");
-        base.initializeDriver();
+        base.initializeDriver(browserName);
         base.initializeLogger(scenario.getName());
     }
 
